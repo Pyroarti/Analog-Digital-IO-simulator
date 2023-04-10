f13faf45a3bf8c8c62d32e634c3eeeaabe7f7e2e
Robert <robert.balulis@hotmail.com>
2023-04-10 21:11:56 +0200

    fixed bug

--- main.py
+++ main.py
@@ -95,27 +95,29 @@ except NameError as error:
 
 """ 680 is 4mA and 3446 is 20mA """
 def sim_higher(app):
+    t = 0
     while True:
-        value1 = 680 + app.t_value
-        value2 = 680 + app.t_value
+        value1 = 680 + t
+        value2 = 680 + t
         outputs.write_single(0, int(value1))
         outputs.write_single(1, int(value2))
         print(value1, value2)
-        t_value += 15
+        t += app.t_value
         sleep(0.1)
 
         if value1 >= 3446 or value2 >= 3446:
-            sim_lower()
+            sim_lower(app)
             break
 
 def sim_lower(app):
+    t = 0
     while True:
-        value1 = 3446 - app.t_value
-        value2 = 3446 - app.t_value
+        value1 = 3446 - t
+        value2 = 3446 - t
         outputs.write_single(0, int(value1))
         outputs.write_single(1, int(value2))
         print(value1, value2)
-        t_value += 15
+        t += app.t_value
         sleep(0.1)
 
         if value1 <= 680 or value2 <= 680:
