5f581c2b1984ec02b7482831a0747b8fcc4b89c0
Robert <robert.balulis@hotmail.com>
2023-04-10 20:01:49 +0200

    made a simple gui

--- main.py
+++ main.py
@@ -1,11 +1,57 @@
 from time import sleep
 
-from widgetlords.pi_spi import *
+import customtkinter
+from PIL import Image
 
-"""680 is 4mA and 3446 is 20mA"""
+try:
+    from widgetlords.pi_spi import *
+except ImportError as error:
+    print(error, "You need to run this code on a raspberry pi")
 
-outputs = Mod2AO()
 
+
+BACKGROUND_IMAGE = "backround.jpg"
+
+class App(customtkinter.CTk):
+    """Class for the main app and main window"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+        self.geometry("400x500")
+        self.title("4-20 analog simulator")
+
+        self.resizable(False, False)
+
+        self.bg_image = customtkinter.CTkImage(Image.open(BACKGROUND_IMAGE),
+                                               size=(400, 780))
+        
+        self.bg_image_label = customtkinter.CTkLabel(self, image=self.bg_image)
+        self.bg_image_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.frame_1 = customtkinter.CTkFrame(master=self, bg_color="transparent")
+        self.frame_1.place(relx=0.5, rely=0.5, anchor="center")
+
+        self.frame_1 = customtkinter.CTkFrame(master=self)
+        self.frame_1.pack(pady=20, padx=40, fill="both", expand=True)
+
+        main_label = customtkinter.CTkLabel(master=self.frame_1, justify=customtkinter.LEFT,
+                                            text="4-20 analog simulator",
+                                            font=customtkinter.CTkFont(size=20, weight="bold"),
+                                            bg_color="transparent")
+        main_label.pack(pady=10, padx=10)
+
+        button_explore = customtkinter.CTkButton(master=self.frame_1,
+                                                 command=lambda: sim_higher(self),
+                                                 text="4-20mA then 20-4mA")
+        button_explore.pack(pady=10, padx=10)
+
+try:
+    outputs = Mod2AO()
+except NameError as error:
+    print(error, "You need to run this code on a raspberry pi")
+
+""" 680 is 4mA and 3446 is 20mA """
 def sim_higher():
     t = 0
     while True:
@@ -13,8 +59,7 @@ def sim_higher():
         value2 = 680 + t
         outputs.write_single(0, int(value1))
         outputs.write_single(1, int(value2))
-        print(value1)
-        print(value2)
+        print(value1, value2)
         t += 15
         sleep(0.1)
 
@@ -30,8 +75,7 @@ def sim_lower():
         value2 = 3446 - t
         outputs.write_single(0, int(value1))
         outputs.write_single(1, int(value2))
-        print(value1)
-        print(value2)
+        print(value1, value2)
         t += 15
         sleep(0.1)
 
@@ -40,8 +84,13 @@ def sim_lower():
 
 
 def main():
-    init()
-    sim_higher()
+    try:
+        init()
+    except NameError as error:
+        print(error, "You need to run this code on a raspberry pi")
+    app = App()
+    app.mainloop()
+
 
 
 if __name__ == "__main__":
