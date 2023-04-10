991079d71b642cc0fa9d56bf05a1ea04629efa48
Robert <robert.balulis@hotmail.com>
2023-04-10 21:04:38 +0200

    Qol

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ except ImportError as error:
 
 BACKGROUND_IMAGE = "backround.jpg"
 
+
+
+
 class App(tk.Tk):
     """Class for the main app and main window"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.geometry("400x500")
+        self.geometry("500x600")
         self.title("4-20 analog simulator")
         self.resizable(False, False)
 
-        self.bg_image = ImageTk.PhotoImage(Image.open(BACKGROUND_IMAGE).resize((400, 780)))
+        self.t_value = 0
+
+        self.bg_image = ImageTk.PhotoImage(Image.open(BACKGROUND_IMAGE).resize((500, 880)))
 
         self.bg_image_label = tk.Label(self, image=self.bg_image)
         self.bg_image_label.place(x=0, y=0, relwidth=1, relheight=1)
@@ -36,29 +41,52 @@ class App(tk.Tk):
                               )
         main_label.pack(pady=10, padx=10)
 
-        button_explore = tk.Button(master=self.frame_1,
-                                   command=sim_higher,
+        button_sim_high_low = tk.Button(master=self.frame_1,
+                                   command=lambda: sim_higher(self),
                                    text="4-20mA then 20-4mA",
                                    width=18)
-        button_explore.pack(pady=10, padx=10)
+        button_sim_high_low.pack(pady=10, padx=10)
 
-        button_explore = tk.Button(master=self.frame_1,
+        button_set_value_4ma = tk.Button(master=self.frame_1,
                                    command=set_value_4ma,
                                    text="Set value to 4mA",
                                    width=18)
-        button_explore.pack(pady=10, padx=10)
+        button_set_value_4ma.pack(pady=10, padx=10)
 
-        button_explore = tk.Button(master=self.frame_1,
+        button_set_value_12ma = tk.Button(master=self.frame_1,
                                    command=set_value_12ma,
                                    text="Set value to 12mA",
                                    width=18)
-        button_explore.pack(pady=10, padx=10)
+        button_set_value_12ma.pack(pady=10, padx=10)
 
-        button_explore = tk.Button(master=self.frame_1,
+        button_set_value_20ma = tk.Button(master=self.frame_1,
                                    command=set_value_20ma,
                                    text="Set value to 20mA",
                                    width=18)
-        button_explore.pack(pady=10, padx=10)
+        button_set_value_20ma.pack(pady=10, padx=10)
+
+        entry_box_label = tk.Label(master=self.frame_1, text="Set the time how fast it goes from 4-20 then 20-4 mA",
+                              font=("Helvetica", 10)
+                              )
+        entry_box_label.pack(pady=5, padx=5)
+
+        entry_box_label_tip = tk.Label(master=self.frame_1, text="Around 10 to 15 is pretty good values to use",
+                              font=("Helvetica", 10)
+                              )
+        entry_box_label_tip.pack(pady=1, padx=1)
+
+        def on_change(_):
+            entered_value = time_entry_box.get()
+            try:
+                self.t_value = int(entered_value)
+            except ValueError:
+                print("Invalid input, please enter an integer value.")
+
+        time_entry_box = tk.Entry(master=self.frame_1)
+        time_entry_box.pack(pady=5, padx=5)
+        time_entry_box.bind("<Return>", on_change)
+
+        
 
 try:
     outputs = Mod2AO()
@@ -66,30 +94,28 @@ except NameError as error:
     print(error, "You need to run this code on a raspberry pi")
 
 """ 680 is 4mA and 3446 is 20mA """
-def sim_higher():
-    t = 0
+def sim_higher(app):
     while True:
-        value1 = 680 + t
-        value2 = 680 + t
+        value1 = 680 + app.t_value
+        value2 = 680 + app.t_value
         outputs.write_single(0, int(value1))
         outputs.write_single(1, int(value2))
         print(value1, value2)
-        t += 15
+        t_value += 15
         sleep(0.1)
 
         if value1 >= 3446 or value2 >= 3446:
             sim_lower()
             break
 
-def sim_lower():
-    t = 0
+def sim_lower(app):
     while True:
-        value1 = 3446 - t
-        value2 = 3446 - t
+        value1 = 3446 - app.t_value
+        value2 = 3446 - app.t_value
         outputs.write_single(0, int(value1))
         outputs.write_single(1, int(value2))
         print(value1, value2)
-        t += 15
+        t_value += 15
         sleep(0.1)
 
         if value1 <= 680 or value2 <= 680:
